c98c13fb1d1226e245d1d17829aa707fecb96c0e
sagar <sagar201993>
2024-06-10 12:58:54 +0200

    created header page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Aboutpage = () => {
+  return <div>about page</div>;
+};
+
+export default Aboutpage;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,26 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <Link href="/" className="inline-flex gap-1">
-        <FontAwesomeIcon className="h-8" icon={faMugHot} />
-        <span className="mt-2">buy me a coffee</span>
-      </Link>
+    <header className="mb-8">
+      <div className="flex justify-between  max-w-2xl mx-auto px-4 py-4">
+        <Link href="/" className="inline-flex gap-1 items-center">
+          <FontAwesomeIcon className="h-8 text-yellow-300" icon={faMugHot} />
+          <span className="mt-2">buy me a coffee</span>
+        </Link>
+        <nav className="mt-2 flex gap-6 items-center">
+          <Link href="/about">About</Link>
+          <Link href="/about">FAQ</Link>
+          <Link href="/contact">Contact</Link>
+          <div className="flex gap-2">
+            <button className="border-2 rounded-full px-4 py-2 ml-4">
+              Login
+            </button>
+            <button className="bg-yellow-300 rounded-full px-4 py-2">
+              Sign Up
+            </button>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
