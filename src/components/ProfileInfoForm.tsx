8b748abdd7de41c1b82104b9f3e065ff435856ab
sagar <sagar201993>
2024-06-13 13:47:36 +0200

    crreated bucket using aws and upload image functunality

--- src/components/ProfileInfoForm.tsx
+++ src/components/ProfileInfoForm.tsx
@@ -1,11 +1,21 @@
 "use client";
-
 import { saveProfile } from "@/actions/profileInfoActions";
+import { uploadToS3 } from "@/actions/uploadActions";
+import { ChangeEvent } from "react";
 
 const ProfileInfoForm = () => {
   async function handlesaveProfile(formData: FormData) {
     const result = await saveProfile(formData);
-    console.log(result);
+  }
+  async function upload(ev: ChangeEvent<HTMLInputElement>) {
+    const target = ev.target as HTMLInputElement;
+    if (target.files?.length) {
+      const file = target.files[0];
+      const formData = new FormData();
+      formData.set("file", file);
+
+      console.log(await uploadToS3(formData));
+    }
   }
   return (
     <form action={handlesaveProfile}>
@@ -13,7 +23,9 @@ const ProfileInfoForm = () => {
         <div className="rounded-full bg-gray-300 w-24 h-24 flex items-center justify-center">
           avatar
         </div>
-        <div className="mt-4">cover image</div>
+        <div className="mt-4">
+          <input type="file" onChange={(ev) => upload(ev)} />
+        </div>
       </div>
 
       <div className="mt-4">
